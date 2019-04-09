7613830a383db46db65b7d48c5329c0cf588afe3
常富文 <1712710548@qq.com>
2019-04-09 14:06:39 +0800

    修改bug

--- src/main/java/com/ch/dto/SolrDto.java
+++ src/main/java/com/ch/dto/SolrDto.java
@@ -8,4 +8,33 @@
 package com.ch.dto;
 
 public class SolrDto {
+    private Integer start;
+
+    private  Integer rows;
+
+    private  String condition;
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
 }
