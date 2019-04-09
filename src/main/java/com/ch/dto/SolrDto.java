b01d3bc99d7e43db5f59daefe736d2990d55e608
常富文 <1712710548@qq.com>
2019-04-09 14:04:57 +0800

    修改bug

--- src/main/java/com/ch/dto/SolrDto.java
+++ src/main/java/com/ch/dto/SolrDto.java
@@ -1,17 +1,11 @@
 /**
  * Author: 常富文
- * Date:   2019/4/9 9:13
- * Description: 网站首页搜索条件
+ * Date:   2019/4/9 11:59
+ * Description: 搜索条件
  */
 
 
 package com.ch.dto;
 
-public class Condition {
-
-    private Integer start;
-
-    private Integer rows;
-
-    private  String 
+public class SolrDto {
 }
