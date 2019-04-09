60d9b29a0df66928c261333a90d46b73b8882fa1
常富文 <1712710548@qq.com>
2019-04-09 10:35:04 +0800

    修改网站首页接口

--- src/main/java/com/ch/controller/ViewGoodsDetailsController.java
+++ src/main/java/com/ch/controller/ViewGoodsDetailsController.java
@@ -7,5 +7,39 @@
 
 package com.ch.controller;
 
+import com.ch.base.ResponseResult;
+import com.ch.service.ViewGoodsDetailsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("goods")
+@Api(value = "商品详情",description = "商品详情")
 public class ViewGoodsDetailsController {
+
+
+   @Autowired
+   ViewGoodsDetailsService viewGoodsDetailsService;
+   private static final Logger LOGGER = LogManager.getLogger(ViewGoodsDetailsController.class);
+
+   @ApiOperation("展示商品详情")
+   @GetMapping("details")
+   public ResponseResult findGoodsDetailsByGoodsId(Integer goodsId){
+       ResponseResult result = new ResponseResult();
+       try {
+           result = viewGoodsDetailsService.findGoodsDetailsByGoodsId(goodsId);
+       } catch (Exception e) {
+           LOGGER.error("展示商品详情" + e.getMessage(), e);
+           result.setCode(500);
+           result.setError(e.getMessage());
+           result.setError_description("展示商品详情");
+       }
+       return result;
+   }
 }
