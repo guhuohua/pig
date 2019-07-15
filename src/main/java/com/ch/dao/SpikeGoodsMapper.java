2eb9ff979d4dd32ea15a7879692b46f10fc37847
changfuwen <1712710548@qq.com>
2019-07-15 15:04:43 +0800

    no message

--- src/main/java/com/ch/dao/SpikeGoodsMapper.java
+++ src/main/java/com/ch/dao/SpikeGoodsMapper.java
@@ -6,11 +6,15 @@ import java.util.List;
 import com.ch.entity.SpikeGoods;
 import com.ch.entity.SpikeGoodsExample;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpikeGoodsMapper {
     int countByExample(SpikeGoodsExample example);
 
     int deleteByExample(SpikeGoodsExample example);
+
+    int deleteByPrimaryKey(Integer id);
 
     int insert(SpikeGoods record);
 
@@ -18,7 +22,13 @@ public interface SpikeGoodsMapper {
 
     List<SpikeGoods> selectByExample(SpikeGoodsExample example);
 
+    SpikeGoods selectByPrimaryKey(Integer id);
+
     int updateByExampleSelective(@Param("record") SpikeGoods record, @Param("example") SpikeGoodsExample example);
 
     int updateByExample(@Param("record") SpikeGoods record, @Param("example") SpikeGoodsExample example);
+
+    int updateByPrimaryKeySelective(SpikeGoods record);
+
+    int updateByPrimaryKey(SpikeGoods record);
 }
