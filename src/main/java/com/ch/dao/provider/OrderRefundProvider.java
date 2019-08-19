a6d09a718e4fa5b665970c4c8c0a5ec532781189
Kyle.chang <1210949654@qq.com>
2019-08-19 10:49:14 +0800

    no message

--- src/main/java/com/ch/dao/provider/OrderRefundProvider.java
+++ src/main/java/com/ch/dao/provider/OrderRefundProvider.java
@@ -12,11 +12,14 @@ public class OrderRefundProvider {
         String refundId = (String) map.get("refundId");
         Integer refundStatus = (Integer) map.get("refundStatus");
         Integer shopId = (Integer) map.get("shopId");
-        StringBuilder sb = new StringBuilder("select orr.id as refund_id, go.id as order_id, ua.name ,orr.price as refund_price,  " +
-                "orr.real_price, go.pay_date, go.track_number as logistics_number , orr.create_date as refund_date, orr.refund_status " +
-                "from order_refund orr " +
-                "left join goods_order go on orr.id = go.refund_id " +
-                "left join user_address ua on go.delivery_id = ua.id where 1=1");
+        StringBuilder sb = new StringBuilder("select orr.id as refund_id," +
+                "     go.id as order_id," +
+                "    ua.name as user_name," +
+                "   orr.price as refund_price," +
+                " go.track_number as logistics_number" +
+                "   from order_refund orr" +
+                "   left join goods_order go on orr.order_id = go.id" +
+                "   left join user_address ua on go.delivery_id = ua.id where 1=1");
         if (BeanUtils.isNotEmpty(name)) {
             sb.append(" and ua.name  like '%").append(name).append("%'");
         }
